ec61955bd8875908306bd0d3903ac61201749182
Daniel Andrew Gonzalez <danielandrewgonzalez@Daniels-MacBook-Pro.local>
2017-04-05 23:43:14 -0400

    mediocre

--- basicCard.js
+++ basicCard.js
@@ -3,6 +3,8 @@ var inquirer = require("inquirer");
 
 var basic = require("./basic.json");
 
+var score = 0;
+
 function BasicCard (front, back) {
   this.front = front;
   this.back = back;
@@ -10,120 +12,124 @@ function BasicCard (front, back) {
 
 var onlyPresident = new BasicCard("Andrew ... was the only U.S. President who paid off the entire national debt.", "Jackson");
 
-// "Who was the first president of the United States?"
-console.log(onlyPresident.front); 
+// Create a "Prompt" with a series of questions.
+inquirer.prompt([
 
-// "George Washington"
-console.log(onlyPresident.back);
+  // Here we create a basic text prompt.
+  {
+    type: "input",
+    message: "Andrew ... was the only U.S. President who paid off the entire national debt.",
+    name: "name"
+  },
+
+  // Here we ask the user to confirm.
+  {
+    type: "confirm",
+    message: "Are you sure:",
+    name: "confirm",
+    default: true
+
+  }
+
+// Once we are done with all the questions... "then" we do stuff with the answers
+// In this case, we store all of the answers into a "user" object that inquirer makes for us.
+]).then(function(answer) {
+
+
+  // If we log that user as a JSON, we can see how it looks.
+  console.log(JSON.stringify(answer, null, 2));
+
+  // If the user confirms, we displays the user's name and pokemon from the answers.
+  if (answer === onlyPresident.back) {
+      score++;
+      console.log("That is correct!");
+    }
+    else {
+      score--;
+      console.log("The correct answer is " + onlyPresident.back);
+    }
+
+});
+
 
 var brotherlyLove = new BasicCard("The Declaration of Independence was signed in the city of ... on July 4, 1776.", "Philadelphia");
 
-console.log(brotherlyLove.front);
+// // Create a "Prompt" with a series of questions.
+// inquirer.prompt([
 
-console.log(brotherlyLove.back);
+//   // Here we create a basic text prompt.
+//   {
+//     type: "input",
+//     message: "The Declaration of Independence was signed in the city of ... on July 4, 1776.", "Philadelphia",
+//     name: "name"
+//   },
+
+//   // Here we ask the user to confirm.
+//   {
+//     type: "confirm",
+//     message: "Are you sure:",
+//     name: "confirm",
+//     default: true
+
+//   }
+
+// // Once we are done with all the questions... "then" we do stuff with the answers
+// // In this case, we store all of the answers into a "user" object that inquirer makes for us.
+// ]).then(function(answer) {
+
+
+//   // If we log that user as a JSON, we can see how it looks.
+//   console.log(JSON.stringify(answer, null, 2));
+
+//   // If the user confirms, we displays the user's name and pokemon from the answers.
+//   if (answer === brotherlyLove.back) {
+//       score++;
+//       console.log("That is correct!");
+//     }
+//     else {
+//       score--;
+//       console.log("The correct answer is " + brotherlyLove.back);
+//     }
+
+// });
 
 var iknowmyRights = new BasicCard("The Bill of ... are the first ten Amendments of the U.S. Constitution.", "Rights");
 
-console.log(iknowmyRights.front);
+// // Create a "Prompt" with a series of questions.
+// inquirer.prompt([
 
-console.log(iknowmyRights.back);
+//   // Here we create a basic text prompt.
+//   {
+//     type: "input",
+//     message: "The Bill of ... are the first ten Amendments of the U.S. Constitution.", "Rights",
+//     name: "name"
+//   },
 
-// // constructor function for creating player objects
-// function Player(name, position, offense, defense) {
-//   this.name = name;
-//   this.position = position;
-//   this.offense = offense;
-//   this.defense = defense;
-//   // flips a coin: if the the value is equal to 0 then this.offense goes up by one. if the value is equal to 1
-//   this.goodGame = function() {
-//     if (Math.floor(Math.random() * 2) === 0) {
-//       this.offense++;
-//       console.log(this.name + "'s offense has gone up!\n----------");
+//   // Here we ask the user to confirm.
+//   {
+//     type: "confirm",
+//     message: "Are you sure:",
+//     name: "confirm",
+//     default: true
+
+//   }
+
+// // Once we are done with all the questions... "then" we do stuff with the answers
+// // In this case, we store all of the answers into a "user" object that inquirer makes for us.
+// ]).then(function(answer) {
+
+
+//   // If we log that user as a JSON, we can see how it looks.
+//   console.log(JSON.stringify(answer, null, 2));
+
+//   // If the user confirms, we displays the user's name and pokemon from the answers.
+//   if (answer === iknowmyRights.back) {
+//       score++;
+//       console.log("That is correct!");
 //     }
 //     else {
-//       this.defense++;
-//       console.log(this.name + "'s defense has gone up!\n----------");
+//       score--;
+//       console.log("The correct answer is " + iknowmyRights.back);
 //     }
-//   };
-//   this.badGame = function() {
-//     if (Math.floor(Math.random() * 2) === 0) {
-//       this.offense--;
-//       console.log(this.name + "'s offense has gone down!\n----------");
-//     }
-//     else {
-//       this.defense--;
-//       console.log(this.name + "'s defense has gone down!\n----------");
-//     }
-//   };
-//   this.printStats = function() {
-//     console.log("Name: " + this.name + "\nPosition: " + this.position +
-//     "\nOffense: " + this.offense + "\nDefense: " + this.defense + "\n----------");
-//   };
-// }
 
-// // arrays used to contain all of our player objects
-// var starters = [];
-// var subs = [];
-// var team = [];
-
-// // recursive function which will allow the user to create 5 players and then will print each player's stats afterwards
-// var createPlayer = function() {
-//   // if the length of the team array is 8 or higher, no more questions will be asked
-//   if (starters.length + subs.length < 8) {
-//     console.log("\nNEW PLAYER!\n");
-//     inquirer.prompt([
-//       {
-//         name: "name",
-//         message: "Player's Name: "
-//       }, {
-//         name: "position",
-//         message: "Player's position: "
-//       }, {
-//         name: "offense",
-//         message: "Player's Offensive Ability: ",
-//         validate: function(value) {
-//           if (isNaN(value) === false && parseInt(value) > 0 && parseInt(value) <= 10) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       }, {
-//         name: "defense",
-//         message: "Player's Defensive Ability: ",
-//         validate: function(value) {
-//           if (isNaN(value) === false && parseInt(value) > 0 && parseInt(value) <= 10) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       }
-//     ]).then(function(answers) {
-//       // runs the constructor and places the new player object into the variable player.
-//       // turns the offense and defense variables into integers as well with parseInt
-//       var player = new Player(answers.name, answers.position, parseInt(answers.offense), parseInt(answers.defense));
-//       // adds a player to the starters array if there are less than five player objects in it.
-//       // otherwise adds the newest player object to the subs array
-//       if (starters.length < 5) {
-//         starters.push(player);
-//         team.push(player);
-//         console.log(player.name + " added to the starters");
-//       }
-//       else {
-//         subs.push(player);
-//         team.push(player);
-//         console.log(player.name + " added to the subs");
-//       }
-//       // runs the createPlayer function once more
-//       createPlayer();
-//     });
-//   }
-//   else {
-//     // loops through the team array and calls printStats() for each object it contains
-//     for (var i = 0; i < team.length; i++) {
-//       team[i].printStats();
-//     }
-//   }
-// };
-
-// // calls the function createPlayer() to start the code
-// createPlayer();
+// });
